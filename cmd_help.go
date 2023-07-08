dfb37943f42f67a312a797025403791f7d7b6bf9
Joy Baruah <joy.baruah@wireonelabs.com>
2023-07-08 21:53:49 +0530

    ADDED: help and exit command

---
+++ cmd_help.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Available Commands:")
+
+	availableCommands := getCommands()
+	for _, cmd := range availableCommands {
+		fmt.Printf(" %s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println("")
+	return nil
+}
